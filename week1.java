461838d7e77ca1cb2ed467b4f7aefd4c82cae55f
sariasih <115599109+sariasih@users.noreply.github.com>
2023-03-07 04:46:10 +0700

    Add files via upload

---
+++ week1.java
@@ -0,0 +1,11 @@
+package firstproject;
+
+public class week1 {
+	public static void main(String[] args) {
+		
+		System.out.println("Hallo, nama saya sari asih sekar wangi");
+		System.out.println("saya adalah mahasiswa teknik informatika universitas dian nuswantoro");
+		System.out.println("saya sedang menulis program java");
+	}
+
+}
