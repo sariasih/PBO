99999594c8660e0d6a219fcdafd098aa447c0bf6
sariasih <115599109+sariasih@users.noreply.github.com>
2023-03-07 05:10:49 +0700

    Add files via upload

---
+++ week1part2.java
@@ -0,0 +1,18 @@
+package firstproject;
+
+public class week1part2 {
+	public static void main(String[] args) {
+		
+		int diameter=10;
+		double phi=3.14;
+		double luas_lingkaran= phi * diameter/2 * diameter/2;
+		int luas_segitiga= 6 * 8 / 2;
+		int diameter2=5;
+		int tinggi=10;
+		double luas_tabung=phi * diameter2/2 *diameter2/2 *tinggi;
+		System.out.println(luas_lingkaran);
+		System.out.println(luas_segitiga);
+		System.out.println(luas_tabung);
+	}
+
+}
