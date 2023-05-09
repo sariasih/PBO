86ab0f2e7da99690ba50fd5b22065a45d538debb
sariasih <115599109+sariasih@users.noreply.github.com>
2023-05-09 09:24:28 +0700

    Add files via upload

---
+++ uts_A11202113733/MahasiswaBaru.java
@@ -0,0 +1,16 @@
+package uts_A11202113733;
+
+public class MahasiswaBaru extends Mahasiswa {
+	
+	String asalSekolah;
+	
+	boolean ikutOspek( ) {
+		return false;
+		
+	}
+	
+	void infoMahasiswa( ) {
+		
+	}
+
+}
